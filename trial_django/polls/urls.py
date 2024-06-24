8eb2aa443e56a7b40e38ffdf10daf7ab4e5ac763
Berkay Gök <berkaybugragok@gmail.com>
2024-06-24 12:08:39 +0300

    Initial commit

---
+++ trial_django/polls/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = "polls"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('question/add/', views.QuestionCreateView.as_view(), name='question_add'),
+    path('question/<int:pk>/edit/', views.QuestionUpdateView.as_view(), name='question_edit'),
+    path('question/<int:pk>/delete/', views.QuestionDeleteView.as_view(), name='question_delete'),
+]
